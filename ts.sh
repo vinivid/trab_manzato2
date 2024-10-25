09345eefc44e4e4408ead98e17897205c56d5934
ViniVid <frato.vini@gmail.com>
2024-10-25 19:07:28 -0300

    esquizofrenia removida

--- ts.sh
+++
@@ -1,6 +0,0 @@
-array_methods=("bubble" "select" "insert" "shell" "quick" "heap" "merge" "count" "radix")
-
-for method in "$array_methods[@]"
-do 
-    ./koala "$method" < ./casos_teste/ran21.in >> "$method".out
-done
